828b3a7bb8e51e54182bb5937e7a83f30e954725
santiago.ramirez <santiago.ramirezc@sofka.com.co>
2025-07-09 09:43:27 -0500

    initialize Angular application with basic structure and configuration

---
+++ Lab1/my-app-node/app.js
@@ -0,0 +1,6 @@
+// app.js - Primer script en Node.js
+console.log("Hello World");
+
+// Información adicional sobre el entorno
+console.log("Versión de Node.js:", process.version);
+console.log("Plataforma:", process.platform);
